041b7a0bc39a670c42bcad3da472d5aff0391898
danii <daniilgeroldovich@gmail.com>
2022-07-23 17:14:27 +0300

    Add first navigation

--- app/src/main/java/com/example/articleblog/MainActivity.kt
+++ app/src/main/java/com/example/articleblog/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.articleblog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.articleblog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
