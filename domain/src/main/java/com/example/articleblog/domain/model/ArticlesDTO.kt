62bd9f78a7485cd092bf7482ba6c5cf6ea03e7f7
danii <daniilgeroldovich@gmail.com>
2022-08-04 20:39:03 +0300

    Add logic of getting articles to ArticlesRepository

---
+++ domain/src/main/java/com/example/articleblog/domain/model/ArticlesDTO.kt
@@ -0,0 +1,24 @@
+package com.example.articleblog.domain.model
+
+data class ArticlesDTO(
+    val articlesList: List<ArticleDTO>,
+)
+
+data class ArticleDTO(
+    val id: Int,
+    val tittle: String,
+    val text: String,
+    val categories: List<CategoryDTO>,
+    val articleInfo: ArticleInfoDTO,
+)
+
+data class CategoryDTO(
+    val id: Int,
+    val category: String,
+)
+
+data class ArticleInfoDTO(
+    val id: Int,
+    val likes: Int,
+    val views: Int,
+)
--- domain/src/main/java/com/example/articleblog/domain/repository/ArticlesRepository.kt
+++ domain/src/main/java/com/example/articleblog/domain/repository/ArticlesRepository.kt
@@ -1,4 +1,7 @@
 package com.example.articleblog.domain.repository
 
+import com.example.articleblog.domain.model.ArticlesDTO
+
 interface ArticlesRepository {
+    suspend fun getArticles(): ArticlesDTO?
 }
