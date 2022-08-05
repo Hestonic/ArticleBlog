405ca7bd4ed8653686a72c2e0295204ab75c48ee
danii <daniilgeroldovich@gmail.com>
2022-08-05 11:44:06 +0300

    Add injection of dependencies to ArticlesFragment.kt

--- app/src/main/java/com/example/articleblog/ui/fragment/articles/ArticlesFragment.kt
+++ app/src/main/java/com/example/articleblog/ui/fragment/articles/ArticlesFragment.kt
@@ -1,18 +1,29 @@
 package com.example.articleblog.ui.fragment.articles
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.articleblog.App
 import com.example.articleblog.databinding.FragmentArticlesBinding
-import com.example.articleblog.ui.model.Articles
+import javax.inject.Inject
 
 class ArticlesFragment : Fragment() {
     
+    @Inject
+    lateinit var articlesViewModelFactory: ArticlesViewModelFactory
+    
+    private lateinit var viewModel: ArticlesViewModel
     private lateinit var binding: FragmentArticlesBinding
     private val adapter = ArticlesAdapter()
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (requireContext().applicationContext as App).appComponent.inject(this)
+    }
     
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,12 +31,19 @@ class ArticlesFragment : Fragment() {
     ): View {
         binding = FragmentArticlesBinding.inflate(inflater, container, false)
         setupRecyclerview()
-        adapter.setData(Articles.articleList)
+        initViewModel()
+        viewModel.getArticles()
+        viewModel.articlesLiveData.observe(viewLifecycleOwner) { adapter.setData(it.articlesList) }
         return binding.root
     }
     
     private fun setupRecyclerview() {
         binding.recycler.adapter = adapter
         binding.recycler.layoutManager = LinearLayoutManager(requireContext())
+    }
+    
+    private fun initViewModel() {
+        viewModel =
+            ViewModelProvider(this, articlesViewModelFactory)[ArticlesViewModel::class.java]
     }
 }
