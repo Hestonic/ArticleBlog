00c64e6c8f92c66a6ba55707ffb8e7fb70dd4c18
danii <daniilgeroldovich@gmail.com>
2022-08-05 18:00:11 +0300

    Add authorization logic to LoginFragment.kt

--- app/src/main/java/com/example/articleblog/ui/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/example/articleblog/ui/fragment/login/LoginFragment.kt
@@ -4,22 +4,62 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.example.articleblog.App
 import com.example.articleblog.databinding.FragmentLoginBinding
+import com.example.articleblog.data.source.local.SessionManager
+import javax.inject.Inject
 
 class LoginFragment : Fragment() {
     
+    @Inject
+    lateinit var loginViewModelFactory: LoginViewModelFactory
+    @Inject
+    lateinit var sessionManager: SessionManager
+    private lateinit var viewModel: LoginViewModel
     private lateinit var binding: FragmentLoginBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (requireContext().applicationContext as App).appComponent.inject(this)
+    }
     
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+        initViewModel()
         binding.registration.setOnClickListener { navigateToRegistrationFragment() }
-        binding.loginButton.setOnClickListener { navigateToArticlesFragment() }
+        binding.loginButton.setOnClickListener { loginUser() }
+        viewModel.tokenLiveData.observe(viewLifecycleOwner) {
+            sessionManager.saveAuthToken(it)
+            navigateToArticlesFragment()
+        }
+        
+        lifecycleScope.launchWhenStarted {
+            viewModel.loginFailedFlow.collect { errorMessage -> makeToast(errorMessage) }
+        }
         return binding.root
+    }
+    
+    private fun loginUser() {
+        val login = binding.loginInput.text.toString()
+        val password = binding.passwordInput.text.toString()
+        viewModel.loginUser(login = login, password = password)
+    }
+    
+    private fun initViewModel() {
+        viewModel =
+            ViewModelProvider(this, loginViewModelFactory)[LoginViewModel::class.java]
+    }
+    
+    private fun makeToast(text: String) {
+        Toast.makeText(requireContext(), text, Toast.LENGTH_LONG).show()
     }
     
     private fun navigateToArticlesFragment() {
