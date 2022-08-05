ae01e3f8b52904fe986e2c180434a8f6a28e488a
danii <daniilgeroldovich@gmail.com>
2022-08-05 12:00:55 +0300

    Add ArticlesMapper.kt

---
+++ data/src/main/java/com/example/articleblog/data/mapper/ArticlesMapper.kt
@@ -0,0 +1,29 @@
+package com.example.articleblog.data.mapper
+
+import com.example.articleblog.data.source.remote.model.ArticlesResponse
+import com.example.articleblog.domain.model.ArticleDTO
+import com.example.articleblog.domain.model.ArticleInfoDTO
+import com.example.articleblog.domain.model.ArticlesDTO
+import com.example.articleblog.domain.model.CategoryDTO
+
+object ArticlesMapper {
+    
+    fun articlesResponseToDTO(articlesResponse: ArticlesResponse) : ArticlesDTO {
+        val articlesList = articlesResponse.articlesList.map { articleResponse ->
+            ArticleDTO(
+                id = articleResponse.id,
+                tittle = articleResponse.tittle,
+                text = articleResponse.text,
+                categories = articleResponse.categories.map { categoryResponse ->
+                    CategoryDTO(id = categoryResponse.id, category = categoryResponse.category)
+                },
+                articleInfo = ArticleInfoDTO(
+                    id = articleResponse.articleInfo.id,
+                    likes = articleResponse.articleInfo.likes,
+                    views = articleResponse.articleInfo.views
+                ),
+            )
+        }
+        return ArticlesDTO(articlesList = articlesList)
+    }
+}
--- data/src/main/java/com/example/articleblog/data/repository/ArticlesRepositoryImpl.kt
+++ data/src/main/java/com/example/articleblog/data/repository/ArticlesRepositoryImpl.kt
@@ -1,31 +1,15 @@
 package com.example.articleblog.data.repository
 
+import com.example.articleblog.data.mapper.ArticlesMapper
 import com.example.articleblog.data.source.RemoteDataSource
-import com.example.articleblog.domain.model.ArticleDTO
-import com.example.articleblog.domain.model.ArticleInfoDTO
 import com.example.articleblog.domain.model.ArticlesDTO
-import com.example.articleblog.domain.model.CategoryDTO
 import com.example.articleblog.domain.repository.ArticlesRepository
 
 class ArticlesRepositoryImpl(private val remoteDataSource: RemoteDataSource) : ArticlesRepository {
+    
     override suspend fun getArticles(): ArticlesDTO? {
         remoteDataSource.getArticles()?.let { articlesResponse ->
-            val articlesList = articlesResponse.articlesList.map { articleResponse ->
-                ArticleDTO(
-                    id = articleResponse.id,
-                    tittle = articleResponse.tittle,
-                    text = articleResponse.text,
-                    categories = articleResponse.categories.map { categoryResponse ->
-                        CategoryDTO(id = categoryResponse.id, category = categoryResponse.category)
-                    },
-                    articleInfo = ArticleInfoDTO(
-                        id = articleResponse.articleInfo.id,
-                        likes = articleResponse.articleInfo.likes,
-                        views = articleResponse.articleInfo.views
-                    ),
-                )
-            }
-            return ArticlesDTO(articlesList = articlesList)
+            return ArticlesMapper.articlesResponseToDTO(articlesResponse)
         }
         return null
     }
