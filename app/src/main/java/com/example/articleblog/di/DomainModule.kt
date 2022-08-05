ab3ec50d2ca21786e9e38c7bd3f68ee6ec80e488
danii <daniilgeroldovich@gmail.com>
2022-08-05 13:40:05 +0300

    Add di provide authorization use cases

--- app/src/main/java/com/example/articleblog/di/DomainModule.kt
+++ app/src/main/java/com/example/articleblog/di/DomainModule.kt
@@ -1,7 +1,10 @@
 package com.example.articleblog.di
 
 import com.example.articleblog.domain.repository.ArticlesRepository
+import com.example.articleblog.domain.repository.AuthorizationRepository
 import com.example.articleblog.domain.usecase.GetArticlesUseCase
+import com.example.articleblog.domain.usecase.LoginUserUseCase
+import com.example.articleblog.domain.usecase.RegisterUserUseCase
 import dagger.Module
 import dagger.Provides
 
@@ -11,5 +14,15 @@ class DomainModule {
     @Provides
     fun provideGetArticlesUseCase(articlesRepository: ArticlesRepository): GetArticlesUseCase {
         return GetArticlesUseCase(articlesRepository = articlesRepository)
+    }
+    
+    @Provides
+    fun provideLoginUserUseCase(authorizationRepository: AuthorizationRepository): LoginUserUseCase {
+        return LoginUserUseCase(authorizationRepository = authorizationRepository)
+    }
+    
+    @Provides
+    fun provideRegisterUserUseCase(authorizationRepository: AuthorizationRepository): RegisterUserUseCase {
+        return RegisterUserUseCase(authorizationRepository = authorizationRepository)
     }
 }
