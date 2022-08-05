0cd1031a0ad7164f08f7624751bc95995c628b99
danii <daniilgeroldovich@gmail.com>
2022-08-05 13:14:06 +0300

    Add AuthorizationRepository.kt interface

--- domain/src/main/java/com/example/articleblog/domain/repository/AuthorizationRepository.kt
+++ domain/src/main/java/com/example/articleblog/domain/repository/AuthorizationRepository.kt
@@ -1,4 +1,8 @@
 package com.example.articleblog.domain.repository
 
+import com.example.articleblog.domain.model.LoginDTO
+
 interface AuthorizationRepository {
+    suspend fun loginUser(loginDTO: LoginDTO): String
+    suspend fun registerUser(registerDTO: LoginDTO): String
 }
