f9836a24de32c2943452eb30996ac256a136a178
danii <daniilgeroldovich@gmail.com>
2022-08-05 17:51:14 +0300

    Add inject functions

--- app/src/main/java/com/example/articleblog/di/AppComponent.kt
+++ app/src/main/java/com/example/articleblog/di/AppComponent.kt
@@ -1,9 +1,13 @@
 package com.example.articleblog.di
 
 import com.example.articleblog.ui.fragment.articles.ArticlesFragment
+import com.example.articleblog.ui.fragment.login.LoginFragment
+import com.example.articleblog.ui.fragment.registration.RegistrationFragment
 import dagger.Component
 
 @Component(modules = [AppModule::class, DataModule::class, DomainModule::class])
 interface AppComponent {
     fun inject(articlesFragment: ArticlesFragment)
+    fun inject(loginFragment: LoginFragment)
+    fun inject(registrationFragment: RegistrationFragment)
 }
