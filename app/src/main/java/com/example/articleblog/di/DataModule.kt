4e6c53cfdbe40911b6351d974e4479b7a0265888
danii <daniilgeroldovich@gmail.com>
2022-08-05 11:40:33 +0300

    Add di DataModule.kt

---
+++ app/src/main/java/com/example/articleblog/di/DataModule.kt
@@ -0,0 +1,36 @@
+package com.example.articleblog.di
+
+import com.example.articleblog.data.repository.ArticlesRepositoryImpl
+import com.example.articleblog.data.repository.AuthorizationRepositoryImpl
+import com.example.articleblog.data.repository.UserRepositoryImpl
+import com.example.articleblog.data.source.RemoteDataSource
+import com.example.articleblog.data.source.remote.ArticleService
+import com.example.articleblog.domain.repository.ArticlesRepository
+import com.example.articleblog.domain.repository.AuthorizationRepository
+import com.example.articleblog.domain.repository.UserRepository
+import dagger.Module
+import dagger.Provides
+
+@Module(includes = [NetworkModule::class])
+class DataModule {
+    
+    @Provides
+    fun provideRemoteDataSource(api: ArticleService) : RemoteDataSource {
+        return RemoteDataSource(api = api)
+    }
+    
+    @Provides
+    fun provideArticlesRepository(remoteDataSource: RemoteDataSource) : ArticlesRepository {
+        return ArticlesRepositoryImpl(remoteDataSource = remoteDataSource)
+    }
+    
+    @Provides
+    fun provideUserRepository(remoteDataSource: RemoteDataSource) : UserRepository {
+        return UserRepositoryImpl(remoteDataSource = remoteDataSource)
+    }
+    
+    @Provides
+    fun provideAuthorizationRepository(remoteDataSource: RemoteDataSource) : AuthorizationRepository {
+        return AuthorizationRepositoryImpl(remoteDataSource = remoteDataSource)
+    }
+}
