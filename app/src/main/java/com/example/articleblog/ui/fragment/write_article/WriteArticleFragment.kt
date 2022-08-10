e737ca63190df6dc8aad2003181e055713e3f7d2
danii <daniilgeroldovich@gmail.com>
2022-08-10 11:24:36 +0300

    Add logic of displaying information on WriteArticleFragment.kt

--- app/src/main/java/com/example/articleblog/ui/fragment/write_article/WriteArticleFragment.kt
+++ app/src/main/java/com/example/articleblog/ui/fragment/write_article/WriteArticleFragment.kt
@@ -7,18 +7,31 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.articleblog.App
 import com.example.articleblog.R
 import com.example.articleblog.databinding.FragmentWriteArticleBinding
 import com.example.articleblog.ui.fragment.article_details.ArticleDetailsAdapter
 import com.example.articleblog.ui.model.CategoryUiModel
+import javax.inject.Inject
 
-// TODO: Сделать сохранение состояния написанной статьи
 class WriteArticleFragment : Fragment() {
     
     private lateinit var binding: FragmentWriteArticleBinding
     private val categoryAdapter = ArticleDetailsAdapter()
     private val categoriesList: MutableList<CategoryUiModel> = mutableListOf()
+    lateinit var viewModel: WriteArticleViewModel
+    
+    @Inject
+    lateinit var writeArticleViewModelFactory: WriteArticleViewModelFactory
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (requireContext().applicationContext as App).appComponent.inject(this)
+        initViewModel()
+        viewModel.getAllCategories()
+    }
     
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,19 +40,25 @@ class WriteArticleFragment : Fragment() {
         binding = FragmentWriteArticleBinding.inflate(inflater, container, false)
         setupRecycler()
         
+        viewModel.categoriesLiveData.observe(viewLifecycleOwner) {
+            val adapterItems = ArrayAdapter(requireContext(), R.layout.item_auto_complete, it)
+            binding.autoCompleteCategory.setAdapter(adapterItems)
+        }
         
-        val items = listOf("Fiction", "Design", "Android", "Strategy", "Biography")
-        val adapterItems = ArrayAdapter(requireContext(), R.layout.item_auto_complete, items)
-        binding.autoCompleteCategory.setAdapter(adapterItems)
         binding.autoCompleteCategory.setOnItemClickListener { adapterView, _, position, _ ->
             val item = adapterView.getItemAtPosition(position).toString()
             Toast.makeText(requireContext(), "$position: $item", Toast.LENGTH_SHORT).show()
             categoriesList.add(CategoryUiModel(id = position, category = item))
             categoryAdapter.setData(categoriesList)
-//            binding.autoCompleteCategory.setText("")
+            binding.autoCompleteCategory.setText("")
         }
         
         return binding.root
+    }
+    
+    private fun initViewModel() {
+        viewModel =
+            ViewModelProvider(this, writeArticleViewModelFactory)[WriteArticleViewModel::class.java]
     }
     
     private fun setupRecycler() {
